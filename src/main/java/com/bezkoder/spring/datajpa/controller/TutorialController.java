604e4bd1aa5e6c54ec870ac77fa734b837f6fff0
sangeetha0201 <110171748+sangeetha0201@users.noreply.github.com>
2023-03-03 15:11:37 +0530

    Update TutorialController.java

--- src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.datajpa.model.Tutorial;
 import com.bezkoder.spring.datajpa.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://192.168.5.131:8081")
+@CrossOrigin(origins = "http://13.233.39.80:8080")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
